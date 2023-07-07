36613b9d2a269d48962d71dd07b406ca348058ef
Austin Daniels <88160796+notaustindaniels@users.noreply.github.com>
2023-07-07 18:53:41 +0000

    Progress

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,9 @@
-//import react into the bundle
 import React from "react";
 import ReactDOM from "react-dom";
 
 // include your styles into the webpack bundle
 import "../styles/index.css";
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 //import your own components
 import Home from "./component/home.jsx";
